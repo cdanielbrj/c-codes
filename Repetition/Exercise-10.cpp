25c51b927df15e9725ae2337851b7365e6759e82
Daniel <danielcarvalho726@gmail.com>
2023-03-13 20:07:13 -0300

    Tabuada de 1 até 10

---
+++ Repetition/Exercise-10.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int cont;
+   
+    cont = 1;
+   
+    while (cont <=10)
+    {   cout<<"\n1 x "<<cont<<" = "<<cont*1;
+        cout<<"\n2 x "<<cont<<" = "<<cont*2;
+        cout<<"\n3 x "<<cont<<" = "<<cont*3;
+        cout<<"\n4 x "<<cont<<" = "<<cont*4;
+        cout<<"\n5 x "<<cont<<" = "<<cont*5;
+        cout<<"\n6 x "<<cont<<" = "<<cont*6;
+        cout<<"\n7 x "<<cont<<" = "<<cont*7;
+        cout<<"\n8 x "<<cont<<" = "<<cont*8;
+        cout<<"\n9 x "<<cont<<" = "<<cont*9;
+        cout<<"\n10 x "<<cont<<" = "<<cont*10;
+        cout<<"\n";
+        cont = cont + 1;
+    }
+}
